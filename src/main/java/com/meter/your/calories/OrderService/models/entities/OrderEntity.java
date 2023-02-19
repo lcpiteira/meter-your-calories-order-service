7c9fd8a2bc8d40545745ab272b46d395fb3f55aa
luispiteira <lcpiteira@gmail.com>
2023-02-19 12:48:48 +0000

    Order Controller has been created with route that permits registry the order

--- src/main/java/com/meter/your/calories/OrderService/models/entities/OrderEntity.java
+++ src/main/java/com/meter/your/calories/OrderService/models/entities/OrderEntity.java
@@ -1,24 +1,24 @@
 package com.meter.your.calories.OrderService.models.entities;
 
 
-import com.meter.your.calories.OrderService.models.enums.FoodGroupEnum;
 import jakarta.persistence.*;
-import jakarta.persistence.criteria.Order;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 
 @Entity
-@Table(name = "order")
+@Table(name = "`order`")
 @Getter
 @Setter
 public class OrderEntity {
 
     public OrderEntity(){}
 
-    public OrderEntity(Timestamp orderCreationTs, UserEntity user, Integer totalCalories) {
+    public OrderEntity(LocalDateTime orderCreationTs, UserEntity user, Integer totalCalories) {
         this.orderCreationTs = orderCreationTs;
         this.user = user;
         this.totalCalories = totalCalories;
@@ -29,8 +29,8 @@ public class OrderEntity {
     @Column(name = "ORDER_ID")
     private Long id;
     @Column(name = "ORDER_CREATION_TS")
-    private Timestamp orderCreationTs;
-    @ManyToOne()
+    private LocalDateTime orderCreationTs;
+    @ManyToOne
     @JoinColumn(name = "USER_ID")
     private UserEntity user;
 
--- src/main/java/com/meter/your/calories/OrderService/web/OrderController.java
+++ src/main/java/com/meter/your/calories/OrderService/web/OrderController.java
@@ -1,14 +1,32 @@
 package com.meter.your.calories.OrderService.web;
 
+import com.meter.your.calories.OrderService.models.dto.OrderDto;
+import com.meter.your.calories.OrderService.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
-    @GetMapping("/test")
-    ResponseEntity<String> testGetMethod(){
-        return ResponseEntity.ok("Teste OK");
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/order")
+    ResponseEntity testGetMethod(@RequestBody OrderDto orderDto){
+        try {
+            orderService.registryOrder(orderDto);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return new ResponseEntity<Void>( HttpStatus.OK );
     }
 
 }
